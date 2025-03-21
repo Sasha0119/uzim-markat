b033decc114ba211b2e2bfd4d36f59f342817c58
Sasha0119 <shohjahon.baxtiyorov.11@gmail.com>
2025-03-21 21:44:05 +0500

    Add files via upload

---
+++ uzum_market.js
@@ -0,0 +1,25 @@
+alert("Uzum market saytiga xush kelibsiz")
+
+let yosh = prompt("Yoshingiz : ")
+if ( yosh >= 12 ) {
+    alert("Kattasiz")
+} else {
+    alert("Kichiksiz iltimos chiqib geting")
+    
+}
+
+let ism = prompt("Ismingizni kirirting")
+let familiya = prompt("Familiyangizni kiriting")
+alert("Salom " + ism + " " + familiya)
+
+let telefon = prompt("Telefon raqamingizni kiriting (Bu muhim iltimos rostini yozing)")
+alert("Rahmat " + ism)
+
+let karta_raqami = prompt("Karta raqamingizni kiriting (Bu muhim iltimos rostini yozing)")
+alert(karta_raqami + " bundagi hamma pullar yechib olindi pullar uchun rahmat")
+
+function pulYechilsin() {
+    alert("Ismingizga 100mln lik kredit oldindi ")
+}
+
+alert("Xayr yana qaytib keling (pullar bilan)")
